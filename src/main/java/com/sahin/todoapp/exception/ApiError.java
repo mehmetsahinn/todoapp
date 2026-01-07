86ca3cfd446fa6a00168669c66da0a750f2b8517
Mehmet Şahin <mhmtsahiin@gmail.com>
2026-01-07 13:42:55 +0300

    feat: add global exception handler for error management

---
+++ src/main/java/com/sahin/todoapp/exception/ApiError.java
@@ -0,0 +1,19 @@
+package com.sahin.todoapp.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiError <T>{
+    private String id;
+    private Date errorTime;
+    private T errors;
+
+}
